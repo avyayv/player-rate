853d1050aeecd2258f13ddc071fccfb788e28a3a
Avyay Varadarajan <avyayv@Avyays-MacBook-Pro.local>
2018-01-21 09:05:03 -0800

    began getting data asynchronously

--- gather-data/define_player.py
+++ gather-data/define_player.py
@@ -4,6 +4,8 @@ class Player:
         self.id = id
         self.name = name
         self.current = current
+        self.seasons = []
+        
         players.append(self)
     def getId():
         return self.id
@@ -11,10 +13,38 @@ class Player:
         return self.name
     def current():
         return self.current
+
 def findById(id):
     for player in players:
         if player.id == id:
             return player
+
+class Season:
+    def __init__(self, playerid, teamid, teamabr, age, gp, gs, min, fgm, fga, fgp, threem, threea, threep, ftm, fta, ftp, oreb, dreb, reb, ast, stl, blk, tov, pf, pts):
+        self.playerid = playerid
+        self.teamid = teamid
+        self.teamabr = teamabr
+        self.age = age
+        self.gp = gp
+        self.gs = gs
+        self.min = min
+        self.fgm = fgm
+        self.fga = fga
+        self.fgp = fgp
+        self.threem = threem
+        self.threea = threea
+        self.threep = threep
+        self.ftm = ftm
+        self.oreb = oreb
+        self.dreb = dreb
+        self.reb = reb
+        self.ast = ast
+        self.stl = stl
+        self.blk = blk
+        self.tov = tov
+        self.pf = pf
+        self.pts = pts
+
 def findByName(name):
     # this is not good unless you are debugging something
     for player in players:
--- gather-data/offensive-statistics.py
+++ gather-data/offensive-statistics.py
@@ -1,1 +1,34 @@
 # this file will get player offensive statistics
+import get_ids
+import json
+from requests_futures.sessions import FuturesSession
+
+headers = {
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8,ru;q=0.6',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+}
+
+
+
+for player in get_ids.define_player.players:
+    try:
+        params = (
+            ('LeagueID', '00'),
+            ('PerMode', 'PerGame'),
+            ('PlayerID', player.id)
+        )
+
+        session = FuturesSession()
+        response = session.get("http://stats.nba.com/stats/playerprofilev2", headers=headers, params=params)
+        html = response.result().content
+        data = json.loads(html)
+        for i in data["resultSets"]:
+            for c in i["rowSet"]:
+                season = get_ids.define_player.Season(player.id, c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10],
+                c[11], c[12], c[13], c[14], c[15], c[16], c[17], c[18], c[19],
+                c[20], c[21], c[22], c[23], c[24], c[25], c[26])
+                print(season.pts)
+                player.seasons.append(season)
+    except:
+        print(player.name)
